18bead80821fd0cc97f44fd168e9b41ae22ed4ea
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-06-20 16:26:54 -0400

    refactor(minijinja-lua): encapsulate lua binding logic

--- crates/minijinja-lua/src/lua.rs
+++ crates/minijinja-lua/src/lua.rs
@@ -1,43 +1,75 @@
+// SPDX-License-Identifier: MIT
+
 use std::sync::atomic::{AtomicPtr, Ordering};
 
-thread_local! {
-    static CURRENT_LUA: AtomicPtr<mlua::Lua> = const { AtomicPtr::new(std::ptr::null_mut()) };
-}
+struct LuaGuard(*mut mlua::Lua);
 
-/// Allow access to a [`mlua::Lua`] instance across a `Send + Sync` boundary in module mode.
-///
-/// This code mirrors the [`minijinja-py`](https://github.com/mitsuhiko/minijinja/blob/29ac0b2936eacf83ebf781c52f4f4ffc3add4c52/minijinja-py/src/state.rs) implementation.
-pub(crate) fn with_lua<R, F: FnOnce(&mlua::Lua) -> Result<R, mlua::Error>>(
-    f: F,
-) -> Result<R, mlua::Error> {
-    CURRENT_LUA.with(|handle| {
-        // SAFETY: The stored Lua pointer is only valid within the context of the `bind_lua` call
-        // on the same thread which stored it. Callers must not attempt or otherwise retain
-        // the `&Lua` reference, or any references to it, that could outlive the scope of the
-        // `bind_lua` call.
-        let ptr = unsafe { (handle.load(Ordering::Relaxed) as *const mlua::Lua).as_ref() };
-
-        match ptr {
-            Some(lua) => f(lua),
-            None => Err(mlua::Error::runtime(
-                "mlua::Lua state accessed outside of a render context.",
-            )),
-        }
-    })
-}
-
-/// Invokes a function with the state stashed away.
-///
-/// This code mirrors the [`minijinja-py`](https://github.com/mitsuhiko/minijinja/blob/29ac0b2936eacf83ebf781c52f4f4ffc3add4c52/minijinja-py/src/state.rs) implementation.
-pub(crate) fn bind_lua<R, F: FnOnce() -> R>(lua: &mlua::Lua, f: F) -> R {
-    let old_handle = CURRENT_LUA
-        .with(|handle| handle.swap(lua as *const mlua::Lua as *mut mlua::Lua, Ordering::Relaxed));
-
-    let rv = std::panic::catch_unwind(std::panic::AssertUnwindSafe(f));
-
-    CURRENT_LUA.with(|handle| handle.store(old_handle, Ordering::Relaxed));
-    match rv {
-        Ok(rv) => rv,
-        Err(payload) => std::panic::resume_unwind(payload),
+impl LuaGuard {
+    thread_local! {
+        static LUA_POINTER: AtomicPtr<mlua::Lua> = const { AtomicPtr::new(std::ptr::null_mut()) };
     }
+
+    /// Create a new guard, swapping the stored pointer with a pointer of the provided
+    /// `&mlua::Lua`. The original pointer will be restored when the guard is dropped.
+    fn new(lua: &mlua::Lua) -> Self {
+        let ptr = Self::LUA_POINTER.with(|handle| {
+            handle.swap(lua as *const mlua::Lua as *mut mlua::Lua, Ordering::Relaxed)
+        });
+
+        Self(ptr)
+    }
+
+    /// Invoke a function with access to a reference to the stored Lua pointer.
+    fn with_lua<R, F>(f: F) -> Result<R, mlua::Error>
+    where
+        F: FnOnce(&mlua::Lua) -> Result<R, mlua::Error>,
+    {
+        Self::LUA_POINTER.with(|handle| {
+            let ptr = handle.load(Ordering::Relaxed) as *const mlua::Lua;
+
+            // SAFETY: The stored Lua pointer is only valid within the context of the `bind_lua`
+            // call on the same thread which stored it, and the Lua reference must not outlive the
+            // closure.
+            let lua = unsafe { ptr.as_ref() }.ok_or_else(|| {
+                mlua::Error::runtime("mlua::Lua state accessed outside of a render context.")
+            })?;
+
+            f(lua)
+        })
+    }
+
+    /// Store a pointer to an `&mlua::Lua` reference and execute a function
+    fn bind_lua<R, F>(lua: &mlua::Lua, f: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        let _guard = Self::new(lua);
+        f()
+    }
+}
+
+impl Drop for LuaGuard {
+    fn drop(&mut self) {
+        Self::LUA_POINTER.with(|handle| handle.store(self.0, Ordering::Relaxed));
+    }
+}
+
+/// Allow access to an [`mlua::Lua`] reference across a `Send + Sync` boundary in module mode.
+///
+/// This code mirrors the [`minijinja-py`](https://github.com/mitsuhiko/minijinja/blob/29ac0b2936eacf83ebf781c52f4f4ffc3add4c52/minijinja-py/src/state.rs) implementation.
+pub(crate) fn with_lua<R, F>(f: F) -> Result<R, mlua::Error>
+where
+    F: FnOnce(&mlua::Lua) -> Result<R, mlua::Error>,
+{
+    LuaGuard::with_lua(f)
+}
+
+/// Invokes a function with the Lua state stashed away.
+///
+/// This code mirrors the [`minijinja-py`](https://github.com/mitsuhiko/minijinja/blob/29ac0b2936eacf83ebf781c52f4f4ffc3add4c52/minijinja-py/src/state.rs) implementation.
+pub(crate) fn bind_lua<R, F>(lua: &mlua::Lua, f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    LuaGuard::bind_lua(lua, f)
 }
