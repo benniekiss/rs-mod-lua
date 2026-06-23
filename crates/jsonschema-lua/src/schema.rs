c98065a2ad41e36aecff7f6e36fe6b7efdbde73d
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-06-23 14:22:28 -0400

    wip

--- crates/jsonschema-lua/src/schema.rs
+++ crates/jsonschema-lua/src/schema.rs
@@ -1,4 +1,5 @@
 use mlua::LuaSerdeExt;
+use rsjson_lua::config::EncodeConfig;
 
 use crate::lua::*;
 
@@ -7,36 +8,40 @@ fn jsonschema_meta_lua(lua: &mlua::Lua) -> mlua::Result<mlua::Table> {
 
     table.set(
         "is_valid",
-        lua.create_function(|_, schema: String| -> mlua::Result<bool> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua, (schema, options): (mlua::Value, Option<EncodeConfig>)| -> mlua::Result<bool> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            Ok(jsonschema::meta::is_valid(&schema_val))
-        })?,
+                Ok(jsonschema::meta::is_valid(&schema_val))
+            },
+        )?,
     )?;
 
     table.set(
         "validate",
-        lua.create_function(|_, schema: String| -> mlua::Result<()> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua, (schema, options): (mlua::Value, Option<EncodeConfig>)| -> mlua::Result<()> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            jsonschema::meta::validate(&schema_val)
-                .map_err(|err| mlua::Error::external(err.to_owned()))
-        })?,
+                jsonschema::meta::validate(&schema_val)
+                    .map_err(|err| mlua::Error::external(err.to_owned()))
+            },
+        )?,
     )?;
 
     table.set(
         "validator_for",
-        lua.create_function(|_, schema: String| -> mlua::Result<LuaValidator> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua,
+             (schema, options): (mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<LuaValidator> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let validator =
-                jsonschema::meta::validator_for(&schema_val).map_err(mlua::Error::external)?;
-
-            Ok(LuaValidator::new(validator.as_ref().clone()))
-        })?,
+                jsonschema::meta::validator_for(&schema_val)
+                    .map(|v| v.into())
+                    .map_err(mlua::Error::external)
+            },
+        )?,
     )?;
 
     Ok(table)
@@ -48,58 +53,66 @@ fn jsonschema_async_lua(lua: &mlua::Lua) -> mlua::Result<mlua::Table> {
 
     table.set(
         "validator_for",
-        lua.create_async_function(async |_, schema: String| -> mlua::Result<LuaValidator> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_async_function(
+            async |lua,
+                   (schema, options): (mlua::Value, Option<EncodeConfig>)|
+                   -> mlua::Result<LuaValidator> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let validator = jsonschema::async_validator_for(&schema_val)
-                .await
-                .map_err(mlua::Error::external)?;
-
-            Ok(LuaValidator::new(validator))
-        })?,
+                jsonschema::async_validator_for(&schema_val)
+                    .await
+                    .map(|v| v.into())
+                    .map_err(mlua::Error::external)
+            },
+        )?,
     )?;
 
     table.set(
         "validator_map_for",
-        lua.create_async_function(async |_, schema: String| -> mlua::Result<LuaValidatorMap> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_async_function(
+            async |lua,
+                   (schema, options): (mlua::Value, Option<EncodeConfig>)|
+                   -> mlua::Result<LuaValidatorMap> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let validator_map = jsonschema::async_validator_map_for(&schema_val)
-                .await
-                .map_err(mlua::Error::external)?;
-
-            Ok(LuaValidatorMap::new(validator_map))
-        })?,
+                jsonschema::async_validator_map_for(&schema_val)
+                    .await
+                    .map(|v| v.into())
+                    .map_err(mlua::Error::external)
+            },
+        )?,
     )?;
 
     table.set(
         "bundle",
-        lua.create_async_function(async |lua, schema: String| -> mlua::Result<mlua::Value> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_async_function(
+            async |lua,
+                   (schema, options): (mlua::Value, Option<EncodeConfig>)|
+                   -> mlua::Result<mlua::Value> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let bundle = jsonschema::async_bundle(&schema_val)
-                .await
-                .map_err(mlua::Error::external)?;
-
-            lua.to_value(&bundle)
-        })?,
+                jsonschema::async_bundle(&schema_val)
+                    .await
+                    .map_err(mlua::Error::external)
+                    .and_then(|bundle| lua.to_value(&bundle))
+            },
+        )?,
     )?;
 
     table.set(
         "dereference",
-        lua.create_async_function(async |lua, schema: String| -> mlua::Result<mlua::Value> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_async_function(
+            async |lua,
+                   (schema, options): (mlua::Value, Option<EncodeConfig>)|
+                   -> mlua::Result<mlua::Value> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let reference = jsonschema::async_dereference(&schema_val)
-                .await
-                .map_err(mlua::Error::external)?;
-
-            lua.to_value(&reference)
-        })?,
+                jsonschema::async_dereference(&schema_val)
+                    .await
+                    .map_err(mlua::Error::external)
+                    .and_then(|reference| lua.to_value(&reference))
+            },
+        )?,
     )?;
 
     Ok(table)
@@ -116,12 +129,12 @@ pub(crate) fn jsonschema_lua(lua: &mlua::Lua) -> mlua::Result<mlua::Table> {
     table.set(
         "is_valid",
         lua.create_function(
-            |_, (schema, json): (String, String)| -> mlua::Result<bool> {
-                let schema_val: serde_json::Value =
-                    serde_json::from_str(&schema).map_err(mlua::Error::external)?;
-
-                let json_val: serde_json::Value =
-                    serde_json::from_str(&json).map_err(mlua::Error::external)?;
+            |lua,
+             (schema, json, options): (mlua::Value, mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<bool> {
+                let options = options.unwrap_or_default();
+                let schema_val = lua.from_value_with(schema, *options)?;
+                let json_val = lua.from_value_with(json, *options)?;
 
                 Ok(jsonschema::is_valid(&schema_val, &json_val))
             },
@@ -130,84 +143,93 @@ pub(crate) fn jsonschema_lua(lua: &mlua::Lua) -> mlua::Result<mlua::Table> {
 
     table.set(
         "validate",
-        lua.create_function(|_, (schema, json): (String, String)| -> mlua::Result<()> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua,
+             (schema, json, options): (mlua::Value, mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<()> {
+                let options = options.unwrap_or_default();
+                let schema_val = lua.from_value_with(schema, *options)?;
+                let json_val = lua.from_value_with(json, *options)?;
 
-            let json_val: serde_json::Value =
-                serde_json::from_str(&json).map_err(mlua::Error::external)?;
-
-            jsonschema::validate(&schema_val, &json_val)
-                .map_err(|err| mlua::Error::external(err.to_owned()))
-        })?,
+                jsonschema::validate(&schema_val, &json_val)
+                    .map_err(|err| mlua::Error::external(err.to_owned()))
+            },
+        )?,
     )?;
 
     table.set(
         "evaluate",
         lua.create_function(
-            |_, (schema, json): (String, String)| -> mlua::Result<LuaEvaluation> {
-                let schema_val: serde_json::Value =
-                    serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+            |lua,
+             (schema, json, options): (mlua::Value, mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<LuaEvaluation> {
+                let options = options.unwrap_or_default();
+                let schema_val = lua.from_value_with(schema, *options)?;
+                let json_val = lua.from_value_with(json, *options)?;
 
-                let json_val: serde_json::Value =
-                    serde_json::from_str(&json).map_err(mlua::Error::external)?;
-
-                let evaluation: jsonschema::Evaluation =
-                    jsonschema::evaluate(&schema_val, &json_val);
-
-                Ok(LuaEvaluation::new(evaluation))
+                Ok(jsonschema::evaluate(&schema_val, &json_val).into())
             },
         )?,
     )?;
 
     table.set(
         "validator_for",
-        lua.create_function(|_, schema: String| -> mlua::Result<LuaValidator> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua,
+             (schema, options): (mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<LuaValidator> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let validator =
-                jsonschema::validator_for(&schema_val).map_err(mlua::Error::external)?;
-
-            Ok(LuaValidator::new(validator))
-        })?,
+                jsonschema::validator_for(&schema_val)
+                    .map(|v| v.into())
+                    .map_err(mlua::Error::external)
+            },
+        )?,
     )?;
 
     table.set(
         "validator_map_for",
-        lua.create_function(|_, schema: String| -> mlua::Result<LuaValidatorMap> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua,
+             (schema, options): (mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<LuaValidatorMap> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let validator_map =
-                jsonschema::validator_map_for(&schema_val).map_err(mlua::Error::external)?;
-
-            Ok(LuaValidatorMap::new(validator_map))
-        })?,
+                jsonschema::validator_map_for(&schema_val)
+                    .map(|v| v.into())
+                    .map_err(mlua::Error::external)
+            },
+        )?,
     )?;
 
     table.set(
         "bundle",
-        lua.create_function(|lua, schema: String| -> mlua::Result<mlua::Value> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua,
+             (schema, options): (mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<mlua::Value> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let bundle = jsonschema::bundle(&schema_val).map_err(mlua::Error::external)?;
-
-            lua.to_value(&bundle)
-        })?,
+                jsonschema::bundle(&schema_val)
+                    .map_err(mlua::Error::external)
+                    .and_then(|bundle| lua.to_value(&bundle))
+            },
+        )?,
     )?;
 
     table.set(
         "dereference",
-        lua.create_function(|lua, schema: String| -> mlua::Result<mlua::Value> {
-            let schema_val: serde_json::Value =
-                serde_json::from_str(&schema).map_err(mlua::Error::external)?;
+        lua.create_function(
+            |lua,
+             (schema, options): (mlua::Value, Option<EncodeConfig>)|
+             -> mlua::Result<mlua::Value> {
+                let schema_val = lua.from_value_with(schema, *options.unwrap_or_default())?;
 
-            let reference = jsonschema::dereference(&schema_val).map_err(mlua::Error::external)?;
-
-            lua.to_value(&reference)
-        })?,
+                jsonschema::dereference(&schema_val)
+                    .map_err(mlua::Error::external)
+                    .and_then(|reference| lua.to_value(&reference))
+            },
+        )?,
     )?;
 
     Ok(table)
