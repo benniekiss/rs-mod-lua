e4e25d633a9bf5e734941983790fa6c3c9b08e6a
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-07-16 10:10:22 -0400

    expose some settings

--- crates/rsast-lua/src/lib.rs
+++ crates/rsast-lua/src/lib.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+use std::num::NonZeroUsize;
+
 use crate::vm::LuaPestVm;
 
 mod lines;
@@ -10,6 +12,23 @@ mod vm;
 #[cfg_attr(feature = "module", mlua::lua_module(name = "rsast"))]
 pub fn rsast_lua(lua: &mlua::Lua) -> mlua::Result<mlua::Table> {
     let table = lua.create_table()?;
+
+    table.set(
+        "set_call_limit",
+        lua.create_function(|_, limit: Option<usize>| {
+            let limit = limit.and_then(NonZeroUsize::new);
+            pest::set_call_limit(limit);
+            Ok(())
+        })?,
+    )?;
+
+    table.set(
+        "set_error_detail",
+        lua.create_function(|_, enable: bool| {
+            pest::set_error_detail(enable);
+            Ok(())
+        })?,
+    )?;
 
     table.set("Ast", lua.create_proxy::<LuaPestVm>()?)?;
 
